c55b3c05ea58124af33ffbcb23386382baac7259
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 22:14:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Task 1 - Add comments to the code below to predict what will happen when it is run.
+#_________________________________________
+# ON THIS PROGRAM I THINK THAT THE CONSOLE WILL DISPLAY "HI! WHAT'S YOUR NAME?"
+#THEN ON ANOTHER LINE IN WOULD DISPLAY THE VARIABLE ASSIGNED THERE "NAME" AND CONCATENATE THE SENTENCE.
+# ________________________________________
+
+#Task 2 - add comments to the code to explain what the variable(s) are called and where they are used. Make sure to show where the variable.
+#__________________________________________
+#THE VARIABLE IS NAME AND THE STRING IS "Hi! What's your name?" AND THE VARIBALE IS USED IN BETWEEN HI AND ! HOW ARE YOU TODAY? 
+print("Hi! What's your name? \n")
+
+name = "Thomas"
+
+print("Hi " + name + "! How are you today?")
